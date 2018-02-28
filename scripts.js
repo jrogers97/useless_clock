d24cab5a323dc773b0dbbcacbbde652931f82e9a
Jake Rogers <jake@dataplusmath.com>
2018-02-27 20:51:41 -0500

    got rid of jquery

--- scripts.js
+++ scripts.js
@@ -1,5 +1,41 @@
-$(document).ready(function() {
-	setInterval(function() {
+// $(document).ready(function() {
+// 	setInterval(function() {
+// 		let new_date = new Date();
+// 		let hours_24 = new_date.getHours();
+
+// 		let hours;
+// 		if (hours_24 !== 0 && hours_24 !== 12) {
+// 			hours = hours_24 % 12
+// 		} else {
+// 			hours = 12
+// 		}
+
+// 		let minutes = new_date.getMinutes();
+// 		let seconds = new_date.getSeconds();
+	
+// 		var hours_str = hours.toString();
+// 		var minutes_str = minutes.toString();
+// 		var seconds_str = seconds.toString();
+
+// 		$(".hour").removeClass('highlight');
+// 		$(".hour." + hours_str).addClass('highlight');
+
+// 		$('.minute').removeClass('highlight');
+// 		$(".minute." + minutes_str).addClass('highlight');
+
+// 		$('.second').removeClass('highlight');
+// 		$('.second.' + seconds_str).addClass('highlight');
+
+// 		// console.log("hours: ", hours, "\n",
+// 		// 	"minutes: ", minutes, "\n",
+// 		// 	"seconds: ", seconds);
+
+// 	}, 1000);
+
+	
+// });
+
+setInterval(function() {
 		let new_date = new Date();
 		let hours_24 = new_date.getHours();
 
@@ -17,20 +53,18 @@ $(document).ready(function() {
 		var minutes_str = minutes.toString();
 		var seconds_str = seconds.toString();
 
-		$(".hour").removeClass('highlight');
-		$(".hour." + hours_str).addClass('highlight');
+		document.getElementByClass("hour").classList.remove('highlight');
+		document.querySelector(".page." + hours_str).classList.add('highlight');
+		// $(".hour." + hours_str).addClass('highlight');
 
-		$('.minute').removeClass('highlight');
-		$(".minute." + minutes_str).addClass('highlight');
+		// $('.minute').removeClass('highlight');
+		// $(".minute." + minutes_str).addClass('highlight');
 
-		$('.second').removeClass('highlight');
-		$('.second.' + seconds_str).addClass('highlight');
+		// $('.second').removeClass('highlight');
+		// $('.second.' + seconds_str).addClass('highlight');
 
 		// console.log("hours: ", hours, "\n",
 		// 	"minutes: ", minutes, "\n",
 		// 	"seconds: ", seconds);
 
 	}, 1000);
-
-	
-});
